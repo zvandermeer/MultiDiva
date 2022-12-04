af72d3c84739c9568d3e8bc654216aa2d650087c
Owen V <owenvandermeer@gmail.com>
2022-12-03 22:01:22 -0500

    Modify to support loading from C++ DLL

--- MultiDiva-Client.go
+++ MultiDiva-Client.go
@@ -1,5 +1,6 @@
 package main
 
+import "C"
 import (
 	"fmt"
 
@@ -7,27 +8,38 @@ import (
 	connectionManager "github.com/ovandermeer/MultiDiva/managers/connectionManager"
 )
 
-import "C"
-
-//export Init
-func Init() {
+//export MultiDivaInit
+func MultiDivaInit() {
 	fmt.Println("[MultiDiva] Welcome to MultiDiva v0.0.2!")
 	cfg := configManager.LoadConfig()
 	connectionManager.Connect(cfg)
 	fmt.Println("Closing!")
 }
 
-//export OnFrame
-func OnFrame() {
+//export MainLoop
+func MainLoop() {
 	go connectionManager.ReceiveScore()
 	go connectionManager.SendScore()
 }
 
+//export SongUpdate
+func SongUpdate(songID C.int, is_practice bool) {
+	go fmt.Println("Received")
+	go fmt.Println(songID)
+	go fmt.Println(is_practice)
+}
+
+//export MultiDivaDispose
+func MultiDivaDispose() {
+	connectionManager.CloseClient()
+}
+
 // use for debugging without diva running
 func main() {
-	Init()
+
+	MultiDivaInit()
 	for {
-		OnFrame()
+		MainLoop()
 	}
 }
 
