8be3d4911ce0ca28b529dabf6df35d0c234f9755
Owen V <owenvandermeer@gmail.com>
2023-05-07 00:49:18 -0400

    Basic in-game UI using sprites

--- MultiDiva-Loader/dllmain.cpp
+++ MultiDiva-Loader/dllmain.cpp
@@ -5,6 +5,7 @@
 // Standard
 #include <string>
 #include <iostream>
+#include <fstream> 
 
 // Deps
 #include "deps/helpers.h"
@@ -26,9 +27,15 @@
 #include <d3d11.h>
 #pragma comment(lib, "d3d11.lib")
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "deps/stb_image.h"
+
+
+
 struct MultiDivaInit_return {
 	bool* connectedToServer;
 	bool* connectedToRoom;
+	int* score;
 };
 
 // Globals
@@ -97,6 +104,7 @@ LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 bool show_menu = true;
 bool show_my_menu = true;
 bool show_canvas = false;
+bool show_ingame_gui = false;
 bool show_fullscreen_menu = false;
 bool keyPressed = false;
 bool publicRoom = true;
@@ -121,7 +129,6 @@ static char serverStatus[256] = "";
 static char serverStatusTooltip[256] = "";
 static char roomStatus[256] = "";
 static char serverVersion[5] = "";
-
 
 // Mod Library
 HMODULE m_Library;
@@ -225,7 +232,6 @@ HOOK(int, __fastcall, _PrintResult, DivaScoreTrigger, int a1) {
 	return original_PrintResult(a1);
 }
 
-
 /*
  * ModLoader
  */
@@ -267,6 +273,58 @@ extern "C" __declspec(dllexport) void OnFrame() {
 	}
 }
 
+// Simple helper function to load an image into a DX11 texture with common settings
+bool LoadTextureFromFile(const char* filename, ID3D11ShaderResourceView** out_srv, int* out_width, int* out_height)
+{
+	// Load from disk into a raw RGBA buffer
+	int image_width = 0;
+	int image_height = 0;
+	unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
+	if (image_data == NULL)
+		return false;
+
+	// Create texture
+	D3D11_TEXTURE2D_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	desc.Width = image_width;
+	desc.Height = image_height;
+	desc.MipLevels = 1;
+	desc.ArraySize = 1;
+	desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	desc.SampleDesc.Count = 1;
+	desc.Usage = D3D11_USAGE_DEFAULT;
+	desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+	desc.CPUAccessFlags = 0;
+
+	ID3D11Texture2D* pTexture = NULL;
+	D3D11_SUBRESOURCE_DATA subResource;
+	subResource.pSysMem = image_data;
+	subResource.SysMemPitch = desc.Width * 4;
+	subResource.SysMemSlicePitch = 0;
+	p_device->CreateTexture2D(&desc, &subResource, &pTexture);
+
+	// Create texture view
+	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+	ZeroMemory(&srvDesc, sizeof(srvDesc));
+	srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	srvDesc.Texture2D.MipLevels = desc.MipLevels;
+	srvDesc.Texture2D.MostDetailedMip = 0;
+	p_device->CreateShaderResourceView(pTexture, &srvDesc, out_srv);
+	pTexture->Release();
+
+	*out_width = image_width;
+	*out_height = image_height;
+	stbi_image_free(image_data);
+
+	return true;
+}
+
+int my_image_width = 0;
+int my_image_height = 0;
+ID3D11ShaderResourceView* coolTexture = NULL;
+ID3D11ShaderResourceView* myNumbers[10];
+
 static long __stdcall detour_present(IDXGISwapChain* p_swap_chain, UINT sync_interval, UINT flags) {
 	if (!init) {
 		if (SUCCEEDED(p_swap_chain->GetDevice(__uuidof(ID3D11Device), (void**)&p_device)))
@@ -294,6 +352,15 @@ static long __stdcall detour_present(IDXGISwapChain* p_swap_chain, UINT sync_int
 			ImGui_ImplWin32_Init(window);
 			ImGui_ImplDX11_Init(p_device, p_context);
 			init = true;
+
+			bool epicBool = LoadTextureFromFile("mods\\MultiDiva\\img\\cool.png", &coolTexture, &my_image_width, &my_image_height);
+			IM_ASSERT(epicBool);
+
+			for (int i = 0; i < 10; i++) {
+				std::string s = "mods\\MultiDiva\\img\\num\\" + std::to_string(i) + ".png";
+				bool newEpicBool = LoadTextureFromFile(s.c_str(), &myNumbers[i], &my_image_width, &my_image_height);
+				IM_ASSERT(newEpicBool);
+			}
 		}
 		else
 			return p_present(p_swap_chain, sync_interval, flags);
@@ -323,11 +390,14 @@ static long __stdcall detour_present(IDXGISwapChain* p_swap_chain, UINT sync_int
 	//	ImGui::End();
 	//}
 
+
+
 	if (show_fullscreen_menu) {
 		static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings;
 		const ImGuiViewport* viewport = ImGui::GetMainViewport();
 		ImGui::SetNextWindowPos(viewport->Pos);
 		ImGui::SetNextWindowSize(viewport->Size);
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.08f, 0.08f, 0.08f, 0.95f));
 		ImGui::Begin("Test Fullscreen", &show_fullscreen_menu, flags);
 		ImGui::NewLine();
 		ImGui::Indent();
@@ -398,6 +468,31 @@ static long __stdcall detour_present(IDXGISwapChain* p_swap_chain, UINT sync_int
 		if (!*connectedToServer) {
 			ImGui::EndDisabled();
 		}
+		if (ImGui::CollapsingHeader("Funny Pictures")) {
+			ImGui::Text("pointer = %p", coolTexture);
+			ImGui::Text("size = %d x %d", my_image_width, my_image_height);
+			ImGui::Image((void*)coolTexture, ImVec2(my_image_width, my_image_height));
+			for (int i = 0; i < 10; i++) {
+				ImGui::Image((void*)myNumbers[i], ImVec2(32, 32));
+				ImGui::SameLine();
+			}
+			ImGui::Text("Show in game gui?");
+			ImGui::Checkbox("##inGameGUI", &show_ingame_gui);
+		}
+		ImGui::End();
+	}
+
+	if (show_ingame_gui) {
+		static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize;
+		const ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowSize(ImVec2(viewport->Size.x/6.4, viewport->Size.y/9));
+		ImGui::SetNextWindowPos(ImVec2(viewport->Size.x - (viewport->Size.x/5.5), viewport->Pos.y + (viewport->Size.y / 14.4)));
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.08f, 0.08f, 0.08f, 0.20f));
+		ImGui::Begin("In game GUI", &show_ingame_gui, flags);
+		ImGui::Text("UsernameHere");
+		ImGui::Image((void*)coolTexture, ImVec2(my_image_width, my_image_height));
+		
+		ImGui::Text("0000000");
 		ImGui::End();
 	}
 
