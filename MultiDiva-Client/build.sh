3c849a72398fc92222dc8e85248b4e05de2bc90f
Zoey VanderMeer <owenvandermeer@gmail.com>
2023-10-16 11:58:39 -0400

    Update build script to run on macOS

--- MultiDiva-Client/build.sh
+++ MultiDiva-Client/build.sh
@@ -1,1 +1,1 @@
-go build -o ./bin/MultiDiva-Client.dll -buildmode=c-shared
+env CC="x86_64-w64-mingw32-gcc" CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o ./bin/MultiDiva-Client.dll -buildmode=c-shared
